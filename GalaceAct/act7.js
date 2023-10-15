073c74dd4869d2727c38c080313f3f657c7fe474
SEAN <146722120+777galacesean@users.noreply.github.com>
2023-10-15 14:49:17 +0800

    Add files via upload
    Activity 7 & 8

---
+++ GalaceAct/act7.js
@@ -0,0 +1,8 @@
+let pw = 'p@sswOrd@2023';
+
+if(pw.length >= pw.includes("p@ssword")){
+    console.log("valid password");
+}
+else{
+    console.log("Invalid password");
+}
---
+++ GalaceAct/act8.js
@@ -0,0 +1,17 @@
+const favSingers =[ 'SZA', 'Daniel Ceasar', 'Frank Ocean' ];
+
+console.log ( favSingers [0]);
+console.log ( favSingers [1]);
+console.log ( favSingers [2]);
+
+
+const favNumbers = [ '111', '222', '444', '888'];
+
+console.log ( favNumbers [0]);
+console.log ( favNumbers [1]);
+console.log ( favNumbers [2]);
+console.log ( favNumbers [3]);
+
+const mixedArr = [ " string ", ["otherarray"], 123, true];
+
+console.log ( favSingers [0], favSingers [1], favSingers[2], favNumbers [1], mixedArr [3]);
